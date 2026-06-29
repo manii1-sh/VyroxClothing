522c923f8b7d81e023e5e8b5ee07344cbd2282f4
manii1-sh <your-email@example.com>
2026-06-29 21:12:47 +0530

    Rename ssr.js to ssr.mjs to fix Netlify ESM import syntax error

--- netlify/functions/ssr.mjs
+++ netlify/functions/ssr.mjs
@@ -1,5 +1,2 @@
-import serverEntry from "../../dist/server/server.js";
-
-export default async (request, context) => {
-  return serverEntry.fetch(request, {}, context);
-};
+import { handler } from "../../dist/server/server.js";
+export { handler };
